54cc591c5f3c71405bc8e3aa027e837c4cf8696f
maybeiambatman <irtezafall2010@gmail.com>
2014-09-05 18:15:18 -0500

    Problem 1.1 solutions

---
+++ chapter-1/problem-1.1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+# Problem Statement:
+# Implement an algorithm to determine if a string has all unique characters.
+# What if you can not use additional data structures?
+
+def check_if_unique(word):
+	# If length of the word is greater than 26, there MUST be a repetition
+	if len(word) > 26:
+		return False
+	# Sort the letters and check if there's a repetition
+	sorted_word = ''.join(sorted(word))
+	for i in range (len(word)-1):
+		if sorted_word[i] == sorted_word[i+1]:
+			return False
+
+	return True
+
+def main():
+	while(True):
+		input_word = raw_input("Enter your word or hit 'q' to quit: ")
+		if input_word == 'q':
+			break
+		print check_if_unique(input_word)
+
+if __name__=='__main__':
+	main()
