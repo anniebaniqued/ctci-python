fcceff9720fa7b78eba52107fee5c982fd668a03
maybeiambatman <irtezafall2010@gmail.com>
2014-09-05 20:54:33 -0500

    problem 1.2 passing all test cases

--- chapter-1/problem_1_2.py
+++ chapter-1/problem_1_2.py
@@ -6,3 +6,19 @@
 #
 # NOTE: For my personal case, I will simply be implementing a function that
 # reverses any given string
+
+def reverse(word):
+    retval = ''
+    for i in range (len(word)-1, -1, -1):
+        retval += word[i]
+    return retval
+
+def main():
+    while(True):
+        input_word = raw_input("Enter your word or hit 'q' to quit: ")
+        if input_word == 'q':
+            break
+        print reverse(input_word)
+
+if __name__ == '__main__':
+    main()
