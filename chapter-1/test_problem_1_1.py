d6d1fc11c2910131bf44fa6278e4cecfcf64f6f4
maybeiambatman <irtezafall2010@gmail.com>
2014-09-05 19:55:50 -0500

    better renaming files. Added test file for problem 1.1

---
+++ chapter-1/test_problem_1_1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from problem_1.1 import *
+
+def test_check_if_unique_able()
+    assert check_if_unique('able') == True
+
+if __name__ == '__main__':
+    test_check_if_unique_able()
